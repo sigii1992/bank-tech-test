9cfde2604c376a52dc82c83aad34ee99b52eba67
Sigita <sigita.zeb92@gmail.com>
2022-01-04 17:58:48 +0000

    Fix bug

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,7 +2,13 @@ class Statement
   def print_statement(bank_account)
     puts 'date || credit || debit || balance'
     bank_account.reverse_each { |transaction| 
-    puts "#{transaction.date} || #{'%.2f' % transaction.credit} || #{'%.2f' % transaction.debit}|| #{'%.2f' % transaction.acc_balance}"
+    puts "#{transaction.date} || #{formatted_number(transaction.credit)} || #{formatted_number(transaction.debit)}|| #{formatted_number(transaction.acc_balance)}"
   }
   end
+
+  private
+
+    def formatted_number(number)
+      '%.2f' % number if number.is_a?(Numeric)
+    end
 end
